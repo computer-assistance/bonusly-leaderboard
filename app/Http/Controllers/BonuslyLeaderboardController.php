80a34bbf0035e8754744fedff4ce856f456ae4da
nadeem <nadeem@computerassistance.co.uk>
2017-03-22 02:41:54 +0000

    dded layout grid calculations to workout % spliced all erverata into a new object wiith calculated data added itweaked onusly image styling - lots of styling

--- app/Http/Controllers/BonuslyLeaderboardController.php
+++ app/Http/Controllers/BonuslyLeaderboardController.php
@@ -7,65 +7,99 @@ use Illuminate\Http\Request;
 
 class BonuslyLeaderboardController extends Controller
 {
-    public function showBoard() {
-      // dd($request);
-      // echo('hi');
+  public function showBoard() {
+    // dd($request);
+    $data = new \stdClass;
+    // echo('hi');
 
-      // $max = $this->getPercentages($this->setupBonuslyApiCall());
-      $max = $this->getPercentages([0,1,9,5,3,12,32,5,6,12,232,4,34]);
-       dd($max);
-      $data = $this->setupBonuslyApiCall();
+    $data1 = $this->setupBonuslyApiCall();
+    $data = $this->analyseData($this->setupBonuslyApiCall());
+    // $metaData = $this->analyseData([0,1,9,5,3,12,32,5,6,12,232,4,34]);
 
-      return view('leaderboard', compact('data', 'max'));
 
+    // dd($data, $data1);
+
+    return view('leaderboard', compact('data'));
+
+  }
+
+
+
+  // https://bonus.ly/api/v1/analytics/standouts?access_token=e288e7aadf0e48c1d0b3a5b84699e15a
+
+  // 'url' => 'http://monitor.wiseserve.net',
+
+
+  function setupBonuslyApiCall() {
+
+    $browser = new \Buzz\Browser();
+    // dd($browser);
+    $bonsulyApiCall = array(
+      'url'           => 'https://bonus.ly/api/v1/analytics/standouts',
+      'access_token'  => 'e288e7aadf0e48c1d0b3a5b84699e15a'
+
+    );
+
+    // $format = 'json';
+
+    $url = $bonsulyApiCall['url'];
+    $access_token = $bonsulyApiCall['access_token'];
+
+    $apiUrl = $url.'?access_token='.$access_token;
+
+    $hostResponse = $browser->get($apiUrl, []);
+    $content = json_decode($hostResponse->getContent());
+
+    $res = $content->result;
+
+    return $res;
+  }
+
+  function analyseData($data) {
+    $res = [];
+    $hi = null;
+    $values = [];
+    $count = 0;
+    $total = 0;
+    $percentages = [];
+    $size = count($data);
+    // dump($data);
+    foreach ($data as $d) {
+      if ($hi == null || $d->count > $hi) {
+        $hi = $d->count;
+      }
+      $total+= $d->count;
+      $count++;
+    }
+    foreach ($data as $key => $val) {
+      $res2 = new \stdClass;
+      $res2 = $val;
+      $res2->percentage = $this->getPrecent($total, $val->count);
+      array_push($values, $val->count);
+      array_push($res, $res2);
     }
 
+    $res = (object)$res;
 
+    // $res->percentages = $this->getPercentages($total, $values);
 
-      // https://bonus.ly/api/v1/analytics/standouts?access_token=e288e7aadf0e48c1d0b3a5b84699e15a
+    // $res->hi = $hi;
+    // $res->count = $count;
+    // $res->percentages = $values;
+    // $res->total = $total;
+    return (array)$res;
+  }
 
-      // 'url' => 'http://monitor.wiseserve.net',
+  function getPercentages($total, $data) {
+    $numItems = count($data);
+    $ret = [];
+    foreach ($data as $d) {
+      array_push($ret, (int)(($d/$total)*100));
+    }
+    return $ret;
+  }
 
-
-      function setupBonuslyApiCall() {
-
-        $browser = new \Buzz\Browser();
-        // dd($browser);
-        $bonsulyApiCall = array(
-          'url'           => 'https://bonus.ly/api/v1/analytics/standouts',
-          'access_token'  => 'e288e7aadf0e48c1d0b3a5b84699e15a'
-
-        );
-
-        // $format = 'json';
-
-        $url = $bonsulyApiCall['url'];
-        $access_token = $bonsulyApiCall['access_token'];
-
-        $apiUrl = $url.'?access_token='.$access_token;
-        //dd($apiUrl);
-        $hostResponse = $browser->get($apiUrl, []);
-        $content = json_decode($hostResponse->getContent());
-
-        //dd($content);
-        $res = $content->result;
-        // dd($h);
-        return $res;
-      }
-
-      function getPercentages($data) {
-        $res = new \stdClass;
-        $hi = null;
-        $count = 0;
-        foreach ($data as $d) {
-          echo $d;
-          if ($hi == null || $d > $hi) {
-            $hi = $d;
-          }
-          $count++;
-        }
-        $res->hi = $hi;
-        $res->count = $count;
-        return $res;
-      }
+  function getPrecent($numItems, $value) {
+    return (int)(($value/$numItems)*100);
+  }
 }
