ca13ab5eab8382e76a465446af770ebfdb5b9689
nadeem <nadeem@computerassistance.co.uk>
2017-03-23 16:46:27 +0000

    might be the final commit

--- app/Http/Controllers/BonuslyLeaderboardController.php
+++ app/Http/Controllers/BonuslyLeaderboardController.php
@@ -9,61 +9,84 @@ use Illuminate\Http\Request;
 
 class BonuslyLeaderboardController extends Controller
 {
+  function titlecmp($a, $b) {
+  //  dd($b->display_name);
+    return $b->giving_balance - $a->giving_balance;
+  }
   public function showBoard() {
 
 
     if (!Cache::has('giverData') && !Cache::has('receiverData')) {
 
-      $userReceivedTotals = [];
-      $userGivenTotals = [];
+      $givenTotal = 0;
+      $receivedTotal = 0;
 
 
       $giverData = $this->analyseData($this->makeBonuslyApiCall($resource = 'analytics', $id = null, $role = 'giver'));
 
       $receiverData = $this->analyseData($this->makeBonuslyApiCall($resource = 'analytics',$id = null, $role = 'receiver'));
 
-      // foreach ($receiverData as $key => $value) {
-      //   array_push($userReceivedTotals, $this->getUserReceipts($value->user->id));
-      // }
-      //
-      // // dd($userReceivedTotals);
-      //
-      // $sortedTotals = array();
-      //
-      // $keys = array_keys($userReceivedTotals);
-
-      // dd($keys);
-      //
-      // foreach ($userReceivedTotals as $key => $row)
-      // {
-      //     $sortedTotals[$key] = $row['earnings'];
-      // }
-      // array_multisort($sortedTotals, SORT_DESC, $userReceivedTotals);
-
-      // dd($sortedTotals);
-
       $users = $this->getUsers();
 
-      $sortedUsers = null;
-            foreach ($users as $key => $value) {
-              $v = array(&$value);
-              print_r( $v);
-              // array_push($sortedUsers, usort($v,array($this, "cmp")) );
-            }
+      $giverPointsData = [];
+      $giverPointsDataSorted = [];
 
-      // $sortedUsers = usort($users,array($this, "cmp"));
-      dd("sorted users $sortedUsers");
+      $receiverPointsData = [];
+      $receiverPointsDataSorted =[];
+
+      $count = null;
+
+      $givenTotal = null;
+      $receivedTotal = null;
+
+      foreach ($users as $key => $value) {
+        if($value->display_name != 'Welcome') {
+          $giverPointsLoopData['id'] = $value->id;
+          $giverPointsLoopData['profile_pic_url'] = $value->profile_pic_url;
+          $giverPointsLoopData['display_name'] = ucfirst($value->display_name);
+          $giverPointsLoopData['giving_balance'] =  100 - $value->giving_balance;
+
+          array_push($giverPointsData, (object)$giverPointsLoopData);
+
+          $givenTotal += $giverPointsLoopData['giving_balance'];
+
+          $receiverPointsLoopData['id'] = $value->id;
+          $receiverPointsLoopData['profile_pic_url'] = $value->profile_pic_url;
+          $receiverPointsLoopData['display_name'] = ucfirst($value->display_name);
+          $receiverPointsLoopData['earning_balance'] = $value->earning_balance;
+
+          array_push($receiverPointsData, (object)$receiverPointsLoopData);
+
+          $receivedTotal += $receiverPointsLoopData['earning_balance'];
+
+          $count++;
+        }
+      }
+
+      usort($giverPointsData, function($a, $b)
+      {
+          return $b->giving_balance - $a->giving_balance;
+      });
+
+      usort($receiverPointsData, function($a, $b)
+      {
+      return $b->earning_balance - $a->earning_balance;
+      });
+
+      $giverPointsData = array_slice($giverPointsData,0, 10);
+      $receiverPointsData = array_slice($receiverPointsData,0, 10);
+
       $expiresAt = Carbon::now()->addMinutes(0);
 
-      Cache::put('giverData', $giverData, $expiresAt);
-      Cache::put('receiverData', $receiverData, $expiresAt);
-      // dd($users); //, $receiverData);
+      Cache::put('giverPointsData', $giverPointsData, $expiresAt);
+      Cache::put('receiverPointsData', $receiverPointsData, $expiresAt);
     }
     else {
-      $giverData = Cache::get('giverData');
-      $receiverData = Cache::get('receiverData');
+      $giverPointsData = Cache::get('giverPointsData');
+      $receiverPointsData = Cache::get('receiverPointsData');
     }
-    return view('leaderboard', compact('giverData', 'receiverData'));
+
+    return view('leaderboard', compact('giverPointsData', 'receiverPointsData'));
   }
 
   // https://bonus.ly/api/v1/analytics/standouts?access_token=e288e7aadf0e48c1d0b3a5b84699e15a
@@ -115,6 +138,10 @@ class BonuslyLeaderboardController extends Controller
 
   function analyseData($data) {
 
+    if(!is_object($data)) {
+      $data = (object)$data;
+    }
+
     $res = [];
     $hi = null;
     $values = [];
@@ -122,7 +149,6 @@ class BonuslyLeaderboardController extends Controller
     $total = 0;
     $percentages = [];
     $size = count($data);
-    // dump($data);
 
     foreach ($data as $d) {
       if ($hi == null || $d->count > $hi) {
@@ -135,7 +161,7 @@ class BonuslyLeaderboardController extends Controller
     foreach ($data as $key => $val) {
       $res2 = new \stdClass;
       $res2 = $val;
-      $res2->percentage = $this->getPrecent($total, $val->count);
+      $res2->percentage = $this->getPercent($total, $val->count);
       array_push($values, $val->count);
       array_push($res, $res2);
     }
@@ -151,16 +177,89 @@ class BonuslyLeaderboardController extends Controller
     return (array)$res;
   }
 
+//   function analyseUserData($data, $type) {
+//
+//     switch ($type) {
+//       case 'giver':
+//       $k ='giving_balance';
+//         break;
+//
+//       case 'receiver':
+//       $k ='earning_balance';
+//         break;
+//
+//       default:
+//         # code...
+//         break;
+//     }
+//
+//     // if(!is_object($data)) {
+//     //   $data = (object)$data;
+//     // }
+//
+//     $res = [];
+//     $hi = null;
+//     $values = [];
+//     $count = 0;
+//     $count2 = 0;
+//     $total = 0;
+//     $percentages = [];
+//     $size = count($data);
+//     var_dump($data);
+//     foreach ($data as $type => $v) {
+//       $count2++;
+//     }
+//
+//     echo "num arrays = $count2 \n";
+//
+//     foreach ($data as $d) {
+//       foreach ($d as $k => $v) {
+//         echo $v . "\n";
+//         if ($hi == null || $v > $hi) {
+//           $hi = $v;
+//         }
+//         $total+= $v;
+//         $count++;
+//       }
+//     }
+//     die;
+//
+//     foreach ($data as $d) {
+//       $res2 = new \stdClass;
+//       $res2 = $d;
+//       foreach ($d as $k => $v) {
+//         if ($hi == null || $v > $hi) {
+//           $hi = $v;
+//         }
+//       $res2->percentage = $this->getPercent($total, $val->$k);
+//       array_push($values, $val->$k);
+//       array_push($res, $res2);
+//     }
+//
+//     $res = (object)$res;
+//
+//     // $res->percentages = $this->getPercentages($total, $values);
+//
+//     // $res->hi = $hi;
+//     // $res->count = $count;
+//     // $res->percentages = $values;
+//     // $res->total = $total;
+//     return (array)$res;
+//   }
+// }
+
   function getPercentages($total, $data) {
-    $numItems = count($data);
     $ret = [];
-    foreach ($data as $d) {
-      array_push($ret, (int)(($d/$total)*100));
+    foreach ($data as $key => $value) {
+      foreach ($value as $k => $v) {
+        echo $k . '<k | v>' . $v ."|\n";
+      }
+      // array_push($ret, (int)(($value/$total)*100));
     }
     return $ret;
   }
 
-  function getPrecent($numItems, $value) {
+  function getPercent($numItems, $value) {
     return (int)(($value/$numItems)*100);
   }
 
@@ -169,21 +268,40 @@ class BonuslyLeaderboardController extends Controller
   }
 
   function getUserData($id) {
-
-    $returnData = [];
-
     return $this->makeBonuslyApiCall($resource = 'users', $id);
-      // $returnData['id'] = $user->id;
-      // $returnData['name'] = $user->display_name;
-      // $returnData['earnings'] = $user->earning_balance;
-
-      // dd($returnData);
-    return $returnData;
   }
 
-  function cmp($a, $b)
-{
-  echo "$a";
-    return strcmp($a->earning_balance, $b->earning_balance);
-}
+function array_sort($array, $on, $order=SORT_ASC) {
+    $new_array = array();
+    $sortable_array = array();
+
+    if (count($array) > 0) {
+        foreach ($array as $k => $v) {
+            if (is_array($v)) {
+                foreach ($v as $k2 => $v2) {
+                    if ($k2 == $on) {
+                        $sortable_array[$k] = $v2;
+                    }
+                }
+            } else {
+                $sortable_array[$k] = $v;
+            }
+        }
+
+        switch ($order) {
+            case SORT_ASC:
+                asort($sortable_array);
+            break;
+            case SORT_DESC:
+                arsort($sortable_array);
+            break;
+        }
+
+        foreach ($sortable_array as $k => $v) {
+            $new_array[$k] = $array[$k];
+        }
+        return $new_array;
+    }
+  }
+
 }
--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -5,80 +5,103 @@
   <div class="row">
     <div class="col-sm-12">
       <img src="img/bonusly_header_logo_wht-1.png">
+      <img src="img/Computer-Assistance-aifile_white_300x89.png" class="pull-right">
     </div>
-    <div class="col-lg-6 col-md-6 col-sm-6" style="text-align: center;">
+    <div class="col-lg-5 col-lg-offset-" style="text-align: center;">
       <h2>Bonusly Givers</h2>
     </div>
-    <div class="col-lg-6 col-md-6 col-sm-6" style="text-align: center;">
+    <div class="col-lg-5 col-lg-offset-1" style="text-align: center;">
       <h2>Bonusly Recievers</h2>
     </div>
   </div>
 </div>
+
+
+
 <div class="container">
   <div class="row">
-    <div id="leaderboard_bars" class="col-lg-2">
-      @foreach($giverData as $d)
-      <span class="progress-bar-img"><img src="{{ $d->user->profile_pic_url }}"></span>
-      <div class="progress box">
-        <label class="progress_label">{{ ucfirst($d->user->display_name) }}</label>
-        <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="$d->percentage" aria-valuemin="0" aria-valuemax="100" style="width: {{ $d->percentage }}%;">
-          {{ $d->percentage }}%
-        </div>
-      </div>
-      @endforeach
-    </div>
-    <div id="leaderboard_table" class="col-lg-3 col-md-12">
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th></th>
-            <th>Name</th>
-            <th>Score</th>
-            <th style="width: 20px">%</th>
-          </tr>
-          @foreach($giverData as $d)
-          <tr class="player">
-            <td class="pic"><img src="{{ $d->user->profile_pic_url }}"></td>
-            <td class="name">{{ ucfirst($d->user->short_name) }}</td>
-            <td class="score">{{ $d->count }}</td>
-            <td><i class="">{{ $d->percentage  }}</i></td>
-          </tr>
-          @endforeach
-        </tbody>
-      </table>
-    </div>
-    <div id="leaderboard_bars" class="col-lg-2 col-lg-offset-1">
-      @foreach($receiverData as $d)
-      <span class="progress-bar-img"><img src="{{ $d->user->profile_pic_url }}"></span>
-      <div class="progress box">
-        <label class="progress_label">{{ ucfirst($d->user->display_name) }}</label>
-        <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="$d->percentage" aria-valuemin="0" aria-valuemax="100" style="width: {{ $d->percentage }}%;">
-          {{ $d->percentage }}%
-        </div>
-      </div>
-      @endforeach
-    </div>
-    <div id="leaderboard_table" class="col-lg-3 col-md-12">
 
-      <table class="table table-striped">
+    <div id="leaderboard_table" class="col-lg-5 col-lg-offset-">
+      <table class="table">
         <thead>
           <tr>
+            <th>Pos</th>
             <th></th>
             <th>Name</th>
-            <th>Score</th>
-            <th style="width: 20px">%</th>
-          </tr>
-          @foreach($receiverData as $d)
+            <th>Given</th>
+          <tr>
+        </thead>
+        <tbody>
+          @foreach($giverPointsData as $indexKey => $d)
           <tr class="player">
-            <td class="pic"><img src="{{ $d->user->profile_pic_url }}"></td>
-            <td class="name">{{ ucfirst($d->user->short_name) }}</td>
-            <td class="score">{{ $d->count }}</td>
-            <td><i class="">{{ $d->percentage  }}</i></td>
+
+            <th>{{ $indexKey +1 }}</th>
+            <td class="pic"><img src="{{ $d->profile_pic_url }}"></td>
+            <td class="name">{{ $d->display_name }}</td>
+            <td class="score">{{ $d->giving_balance }}</td>
           </tr>
           @endforeach
         </tbody>
       </table>
     </div>
+
+
+    <!-- <div id="leaderboard_bars" class="col-lg-3">
+    @foreach($receiverPointsData as $d)
+      <span class="progress-bar-img"><img src="{{ $d->profile_pic_url }}"></span>
+      <div class="progress box">
+        <label class="progress_label">{{ $d->display_name }}</label>
+        <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="" aria-valuemin="0" aria-valuemax="100" style="width: 50%;">
+          {{ $d->earning_balance }}
+        </div>
+      </div>
+    @endforeach
+
+  </div> -->
+
+
+<div id="leaderboard_table" class="col-lg-5 col-lg-offset-1">
+
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Pos</th>
+        <th></th>
+        <th>Name</th>
+        <th>Received</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach($receiverPointsData as $indexKey => $d)
+      <tr class="player">
+        <th>{{ $indexKey +1 }}</th>
+        <td class="pic"><img src="{{ $d->profile_pic_url }}"></td>
+        <td class="name">{{ $d->display_name }}</td>
+        <td class="score">{{ $d->earning_balance }}</td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+  </table>
+</div>
+
+
+<!--
+
+
+    <div id="leaderboard_bars" class="col-lg-3">
+    @foreach($giverPointsData as $d)
+      <span class="progress-bar-img"><img src="{{ $d->profile_pic_url }}"></span>
+      <div class="progress box">
+        <label class="progress_label">{{ $d->display_name }}</label>
+        <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="" aria-valuemin="0" aria-valuemax="100" style="width: 50%;">
+          {{ $d->giving_balance }}
+        </div>
+      </div>
+    @endforeach
+    </div> -->
+
+
   </div>
 </div>
 @stop
