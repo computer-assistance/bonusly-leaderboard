af7a99328353d327347fd8c0e777893c4ea215db
nadeem <nadeem@computerassistance.co.uk>
2017-03-24 18:45:40 +0000

    hid monthly  totals style tewawks

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -7,34 +7,24 @@
       <img src="img/bonusly_header_logo_wht-1_grad_green2.png">
       <img src="img/Computer-Assistance-aifile_white_300x89_grad_green2.png" class="pull-right">
     </div>
-    <div class="col-lg-4 col-lg-offset-" style="text-align: center;">
+
+    <!-- Junaid said hide monthly totals -->
+     <div class="col-lg-4 col-lg-offset-" style="text-align: center;">
       <h2 class="my_title">Givers</h2>
-      <h3>Total {{ $givenTotal }} points </h3>
+    <!--  <h3>Total {{ $givenTotal }} points </h3>  -->
     </div>
     <div class="col-lg-5 col-lg-offset-3" style="text-align: center;">
-      <h2 class="my_title">Recievers</h2>
-      <h3>Total {{ $receivedTotal }} points </h3>
-
+      <h2 class="my_title">Receivers</h2>
+    <!--  <h3>Total {{ $receivedTotal }} points </h3> -->
     </div>
+
   </div>
 </div>
 
-
-
 <div id="" class="container">
   <div class="row">
+
     <div id="leaderboard_bars" class="col-lg-5">
-      @foreach($receiverPointsData as $d)
-      <span class="progress-bar-img"><img src="{{ $d->profile_pic_url }}"></span>
-      <div class="progress box">
-        <label class="progress_label">{{ $d->display_name }}</label>
-        <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="" aria-valuemin="0" aria-valuemax="100" style="width: {{ $d->receiver_percentage * 1.5 }}%;">
-          {{ $d->earning_balance }}
-        </div>
-      </div>
-      @endforeach
-    </div>
-    <div id="leaderboard_bars" class="col-lg-5 col-lg-offset-2">
       @foreach($giverPointsData as $d)
       <span class="progress-bar-img"><img src="{{ $d->profile_pic_url }}"></span>
       <div class="progress box">
@@ -45,73 +35,75 @@
       </div>
       @endforeach
     </div>
+
+    <div id="leaderboard_bars" class="col-lg-5 col-lg-offset-2">
+      @foreach($receiverPointsData as $d)
+      <span class="progress-bar-img"><img src="{{ $d->profile_pic_url }}"></span>
+      <div class="progress box">
+        <label class="progress_label">{{ $d->display_name }}</label>
+        <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="" aria-valuemin="0" aria-valuemax="100" style="width: {{ $d->receiver_percentage * 1.5 }}%;">
+          {{ $d->earning_balance }}
+        </div>
+      </div>
+      @endforeach
+    </div>
+
   </div>
 </div>
-
-
 
 <div id="tables" class="container">
   <div class="row">
+
     <div id="" class="col-lg-4 col-lg-offset-1">
       <div class="leaderboard_table">
-      <table class="table">
-        <thead>
-          <tr>
-            <th>Pos</th>
-            <th></th>
-            <th>Name</th>
-            <th>Given</th>
-          <tr>
-        </thead>
-        <tbody>
-          @foreach($giverPointsData as $indexKey => $d)
-          <tr class="player">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Pos</th>
+              <th></th>
+              <th>Name</th>
+              <th>Given</th>
+            <tr>
+          </thead>
+          <tbody>
+            @foreach($giverPointsData as $indexKey => $d)
+            <tr class="player">
+              <th>{{ $indexKey +1 }}</th>
+              <td class="pic"><img src="{{ $d->profile_pic_url }}"></td>
+              <td class="name">{{ $d->display_name }}</td>
+              <td class="score">{{ $d->giving_balance }}</td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
 
-            <th>{{ $indexKey +1 }}</th>
-            <td class="pic"><img src="{{ $d->profile_pic_url }}"></td>
-            <td class="name">{{ $d->display_name }}</td>
-            <td class="score">{{ $d->giving_balance }}</td>
-          </tr>
-          @endforeach
-        </tbody>
-      </table>
+      <div id="" class="col-lg-4 col-lg-offset-1">
+        <div class="leaderboard_table">
+          <table class="table">
+            <thead>
+              <tr>
+                <th>Pos</th>
+                <th></th>
+                <th>Name</th>
+                <th>Received</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach($receiverPointsData as $indexKey => $d)
+              <tr class="player">
+                <th>{{ $indexKey +1 }}</th>
+                <td class="pic"><img src="{{ $d->profile_pic_url }}"></td>
+                <td class="name">{{ $d->display_name }}</td>
+                <td class="score">{{ $d->earning_balance }}</td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </table>
+      </div>
     </div>
   </div>
-
-
-
-
-<div id="" class="col-lg-4 col-lg-offset-1">
-  <div class="leaderboard_table">
-
-  <table class="table">
-    <thead>
-      <tr>
-        <th>Pos</th>
-        <th></th>
-        <th>Name</th>
-        <th>Received</th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach($receiverPointsData as $indexKey => $d)
-      <tr class="player">
-        <th>{{ $indexKey +1 }}</th>
-        <td class="pic"><img src="{{ $d->profile_pic_url }}"></td>
-        <td class="name">{{ $d->display_name }}</td>
-        <td class="score">{{ $d->earning_balance }}</td>
-      </tr>
-      @endforeach
-    </tbody>
-  </table>
-  </table>
 </div>
-</div>
-
-
-
-
-
-  </div>
-</div>
-@stop
+  @stop
