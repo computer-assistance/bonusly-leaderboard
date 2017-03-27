d4d45d71d970b8d6963e249186d53a29f404e151
nadeem <nadeem@computerassistance.co.uk>
2017-03-27 09:45:45 +0100

    testing on live 1

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,7 +1,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta http-equiv="refresh" content="600">
+	<meta http-equiv="refresh" content="6">
 
 	<title> Bonusly Leaderboard</title>
 
