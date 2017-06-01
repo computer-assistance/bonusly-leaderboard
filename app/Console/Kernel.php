ccc02d469425e529dbc95228c3335ac68fc774ed
nadeem <nadeem@computerassistance.co.uk>
2017-06-01 19:16:45 +0100

    added new command, aaded it to cron job scheduale

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         //
         Commands\SetPositions::class,
+        Commands\ResetPositionsClasses::class,
     ];
 
     /**
@@ -25,8 +26,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('ResetPositionsClasses')
+        ->everyMinute();
     }
 
     /**
